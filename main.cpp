f36768a9ffe5449fe68d6d31e53abe3f82e6ec05
Serpent7776 <serpent7776@gmail.com>
2019-06-20 17:20:25 +0200

    added consts

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 
 int main(int argc, char *argv[])
 {
-	auto sample_sets_count = std::size_t {1};
-	auto sample_count = std::size_t {1};
+	const auto sample_sets_count = std::size_t {1};
+	const auto sample_count = std::size_t {1};
 	StreamSampler::CStreamSamplerWOR_R0<std::string> sampler {sample_sets_count, sample_count};
 	boost::filesystem::directory_iterator it {"."};
 	for (const auto& p : it) {
 		sampler.AddElement(std::move(p.path().string()));
 	}
-	auto sample_sets = sampler.GetSampleSets();
-	assert(sample_sets.size() == 1);
-	assert(sample_sets[0].size() == 1);
+	const auto sample_sets = sampler.GetSampleSets();
+	assert(sample_sets.size() == sample_sets_count);
+	assert(sample_sets[0].size() == sample_count);
 	const auto& sampled_path = sample_sets[0][0];
 	std::cout << sampled_path << "\n";
 	return 0;
